8a49e13b58df7a7de156022a74d95348e7297ab6
JotaCosendey <joaovictor_barros@hotmail.com>
2021-05-07 17:18:37 -0300

    desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,6 +14,7 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+
   function newId() {
     return Math.random()
   }
